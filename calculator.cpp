7aff4c2f6b9a3b15ba1efbaf95285fcc377d2184
Daniil Dyachenko <daniil.dyachenko.2018@gmail.com>
2024-12-27 17:05:14 +0200

    add a exponentiation and calculation of interest

--- calculator.cpp
+++ calculator.cpp
@@ -2,15 +2,25 @@
 
 int Calculator::Add (double a, double b)
 {
-    return a + b + 0.5;
+	return a + b + 0.5;
 }
 
 int Calculator::Sub (double a, double b)
 {
-    return Add (a, -b);
+    	return Add (a, -b);
 }
 
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+    	return a * b + 0.5;
+}
+
+int Calculator::Pow(double a, double b)
+{
+    	return std::round(std::pow(a, b));
+}
+
+int Calculator::Percent(double a, double b)
+{
+    	return std::round((a * b) / 100);
 }
--- calculator.h
+++ calculator.h
@@ -2,11 +2,13 @@
 #define CALCULATOR_H
 
 class Calculator
-{	
+{
     public:
         int Add (double, double);
         int Sub (double, double);
         int Mul (double, double);
+	int Pow(double, double);
+	int Percent(double, double);
 };
 
 #endif//CALCULATOR_H
